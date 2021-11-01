ac10fe92b20fbaf06d01fc5e3f399d7628d9d8f8
Saxenavidhi <93475008+Saxenavidhi@users.noreply.github.com>
2021-11-01 13:37:04 +0530

    Update one.java

--- one.java
+++ one.java
@@ -2,6 +2,6 @@ import java.util.*;
 public class one{
     public static void main(String[] args)
     {
-        System.out.println("Executing One.java file");
+        System.out.println("Executing one.java file!");
     }
 }
