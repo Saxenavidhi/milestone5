3851fddc65db3c3f23389d307b5239cdccffbeae
Saxenavidhi <v21195@gmail.com>
2021-11-01 13:46:12 +0530

    added sysout

--- one.java
+++ one.java
@@ -3,5 +3,6 @@ public class one{
     public static void main(String[] args)
     {
         System.out.println("Executing one.java file!");
+        System.out.println("Testing pipeline!");
     }
 }
